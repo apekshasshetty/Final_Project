3afbfb338a197d0bebebec6f6fb3915e7b93edab
apekshasshetty <apekshasshetty29@gmail.com>
2025-05-07 20:33:13 +0530

    Trigger build for BigQuery procedure

--- data_pipeline/tests/test_cleaned_data.sql
+++ data_pipeline/tests/test_cleaned_data.sql
@@ -1,4 +1,4 @@
 -- Check NULLs removed from Product Rating
 SELECT COUNT(*) AS null_ratings
 FROM walmart_data.cleaned_data
-WHERE `Product Rating` IS NULL;
+WHERE `Product Ratings` IS NULL;
--- data_pipeline/tests/test_transformed_data.sql
+++ data_pipeline/tests/test_transformed_data.sql
@@ -1,4 +1,4 @@
 -- Check that Price_In_INR was calculated
-SELECT COUNT(*) AS missing_inr_price
+SELECT COUNT(*) AS inr_price_missing
 FROM walmart_data.transformed_data
 WHERE Price_In_INR IS NULL;

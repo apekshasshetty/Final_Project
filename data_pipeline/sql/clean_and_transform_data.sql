196be9c6af2cb47c0ac24d61364a0261f8834f5a
apekshasshetty <apekshasshetty29@gmail.com>
2025-05-07 22:08:04 +0530

    query edits

--- data_pipeline/sql/clean_and_transform_data.sql
+++ data_pipeline/sql/clean_and_transform_data.sql
@@ -1,20 +1,19 @@
--- sql/clean_and_transform_data.sql
 CREATE OR REPLACE PROCEDURE walmart_data.refresh_transformed_data()
 BEGIN
   -- Step 1: Create cleaned_data
   CREATE OR REPLACE TABLE walmart_data.cleaned_data AS
   SELECT
-    Uniq Id,
-    Product Id,
-    Product Price,
-    IFNULL(Product Rating, 0) AS Product Rating,
-    IFNULL(Product Reviews Count, 0) AS Product Reviews Count,
-    IFNULL(Product Category, '') AS Product Category,
-    IFNULL(Product Brand, '') AS Product Brand,
-    Product Name,
-    Product Image Url,
-    IFNULL(Product Description, '') AS Product Description,
-    Product Tags
+    `Uniq Id`,
+    `Product Id`,
+    `Product Price`,
+    IFNULL(`Product Rating`, 0) AS `Product_Rating`,
+    IFNULL(`Product Reviews Count`, 0) AS `Product_Reviews_Count`,
+    IFNULL(`Product Category`, '') AS `Product_Category`,
+    IFNULL(`Product Brand`, '') AS `Product_Brand`,
+    `Product Name`,
+    `Product Image Url`,
+    IFNULL(`Product Description`, '') AS `Product_Description`,
+    `Product Tags`
   FROM
     walmart_data.data;
 
@@ -31,7 +30,7 @@ BEGIN
   )
   SELECT 
     c.*, 
-    c.`Product Price` * r.inr_rate AS Price_In_INR
+    c.`Product Price` * r.inr_rate AS `Price_In_INR`
   FROM walmart_data.cleaned_data c
   CROSS JOIN inr_rate_cte r;
 END;

50988abc642ca0650493d25122ccdb5357b06206
apekshasshetty <apekshasshetty29@gmail.com>
2025-05-07 20:52:31 +0530

    Trigger build for BigQuery procedure

--- data_pipeline/tests/test_transformed_data.sql
+++ data_pipeline/tests/test_transformed_data.sql
@@ -1,4 +1,4 @@
 -- Check that Price_In_INR was calculated
-SELECT COUNT(*) AS inr_price_missing
+SELECT COUNT(*) AS missing_inr_price
 FROM walmart_data.transformed_data
 WHERE Price_In_INR IS NULL;
